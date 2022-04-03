655b0e58df4778fb9b537bba26c5a04343347fe4
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-04-04 00:08:38 +1000

    second day

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,88 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
   return (
-    <div>
+    <div className="mx-auto flex min-h-screen max-w-7xl flex-col py-20 px-10 2xl:px-0">
       <Head>
         <title>NFT Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-4xl font-bold text-red-500">
-        Welcome to the NFT Drop Challenge
+      <h1 className="mb-10 text-4xl font-extralight">
+        The{' '}
+        <span className="font-extrabold underline decoration-pink-600/50">
+          Papafam
+        </span>{' '}
+        NFT Marketplace
       </h1>
+
+      <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400/20">
+        <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          {collections.map((collection) => (
+            <Link href={`/nft/${collection.slug.current}`}>
+              <div className="flex cursor-pointer flex-col items-center transition-all duration-200 hover:scale-105">
+                <img
+                  className="h-96 w-60 rounded-2xl object-cover"
+                  src={urlFor(collection.mainImage).url()}
+                  alt=""
+                />
+                <div className="">
+                  <h2 className="text-3xl">{collection.title}</h2>
+                  <p className="mt-2 text-sm text-gray-400">
+                    {collection.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    mainImage {
+      asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+        current
+      },
+    },
+  }`
+
+  const collections = await sanityClient.fetch(query)
+  // console.log(collections)
+
+  return {
+    props: {
+      collections,
+    },
+  }
+}
